75cb89337c937b423cac738a71e94b99630e5f41
tibor <tib>
2023-06-19 21:02:24 +0200

    Add Libary in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,12 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'production',
     module: {
         rules: [
